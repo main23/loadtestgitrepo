84f4523d867fcff047bcb5c62e221967894cca25
GaneSh <ganeshgamut@gmail.com>
2019-07-14 13:36:34 +0530

    f11

--- Logout.java
+++ Logout.java
@@ -1,6 +1,9 @@
 class Logout {
 	
 	public static void main ( String args[]) {
+
+		System.out.println("Hello GaneSh");
+		System.out.println("Hello GaneSh");
 		System.out.println("Hello GaneSh");
 }
 }
